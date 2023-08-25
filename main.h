7340319355519de49de50dfbfcac0b484ec75a91
Ingabireo <ingabireo64@gmail.com>
2023-08-26 01:33:31 +0200

    changes

--- main.h
+++ main.h
@@ -1,36 +1,33 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/wait.h>
 #include <sys/stat.h>
-#include <errno.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <time.h>
 
-
-int lsh_ctrld(char **args);
-int lsh_cd(char **args);
-int lsh_help(char **args);
 extern char **environ;
-int lsh_exit(char **args);
-int _strcmp(char *s1, char *s2);
-size_t _strncmp(char *s1, char *s2, size_t n);
-int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
-char *_strcat(char *dest, char *src);
+
+int _strcmp(char *str1, char *str2);
+int verify_path(char **arguments);
+char *_strdup(char *str);
+char *append_command(char *dir_path, char *command);
+int exec(char **arguments);
+int fill_args(char *entry, char **arguments);
+int print_numbers(int n);
+int _strlen(const char *string);
+int print_not_found(char **arguments, int counter);
+char *_getenv(char *global_var);
+int _printp(const char *prompt, unsigned int size);
 int _putchar(char c);
+int exist(char *pathname);
+void free_grid(char **grid, int heigth);
+void last_free(char *entry);
+int verify_blt(char **arguments, int exit_stat);
 
-char *_get_path(char **env);
-int _values_path(char **arg, char **env);
-char *_getline_command(void);
-void _getenv(char **env);
-char **tokenize(char *lineptr);
-void _exit_command(char **args, char *lineptr, int _exit);
-int _fork_fun(char **arg, char **av, char **env,
-char *lineptr, int np, int c);
-
-
-#endif /*MAIN_H*/
+#endif
